6989cc8c66ba445e39868ae9f983ecddbb9a1f76
eddyzera <edgar.nascimento@newgo.com.br>
2022-03-21 11:30:02 -0300

    [refactor/feat]: fragmenting header into several pieces and creation page UserCreate

---
+++ src/components/Header/Logo.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+  Text
+} from '@chakra-ui/react'
+
+export const Logo: React.FunctionComponent = () => {
+  return (
+    <Text
+      fontSize="3xl"
+      fontWeight="bold"
+      letterSpacing="tight"
+      w="64"
+    >
+      dashgo
+      <Text as="span" ml="1" color="pink.500">.</Text>
+    </Text>
+  )
+}
---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+  Flex,
+  Box,
+  Text,
+  Avatar
+} from '@chakra-ui/react'
+
+export const Profile: React.FunctionComponent = () => {
+  return (
+    <Flex align="center">
+      <Box mr="4" textAlign="right">
+        <Text>Edgar Silva</Text>
+        <Text color="gray.300" fontSize="small">
+          esilva644@gmail.com
+        </Text>
+      </Box>
+      <Avatar
+        size="md"
+        name="Edgar Silva"
+        src="https://avatars.githubusercontent.com/u/60861927?v=4"
+      />
+    </Flex>
+  )
+}
